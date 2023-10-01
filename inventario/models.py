77903fcbbbb5992f27d6e55e67ded73ba5d7f427
Marin Fernandez <mwfernandezm@gmail.com>
2023-10-01 10:10:19 -0400

    Despues de agregar la tabla Categoria y Producto

--- inventario/models.py
+++ inventario/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Catgoria(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    
+    def __str__(self):
+        return self.nombre
+    
+class ProductUnits(models.TextChoices):
+    UNITS = 'u', 'Unidades'
+    KILOGRAMO = 'kg', 'Kilogramos'
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    categoria = models.ForeignKey(Catgoria, on_delete=models.CASCADE)
+    descripcion = models.TextField
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    unidades = models.CharField(max_length=3, choices=ProductUnits.choices, default=ProductUnits.UNITS)
+    disponible = models.BooleanField(blank=True, default=True)
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
