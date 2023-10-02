084270dce1c49adfe43eec9ce4395804bc6d1e2d
Marin Fernandez <mwfernandezm@gmail.com>
2023-10-02 08:27:11 -0400

    Despues de agregar readme.md

--- inventario/models.py
+++ inventario/models.py
@@ -15,7 +15,7 @@ class ProductUnits(models.TextChoices):
 class Producto(models.Model):
     nombre = models.CharField(max_length=100, unique=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
-    descripcion = models.TextField
+    descripcion = models.TextField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     unidades = models.CharField(max_length=3, choices=ProductUnits.choices, default=ProductUnits.UNITS)
     disponible = models.BooleanField(blank=True, default=True)
